89110c97d0d7246b9a444cb48290879e2536c75c
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-06-04 18:26:45 -0300

    feat: :sparkles: implementing confirmation delete dialog

---
+++ client/src/components/blocks/DeleteConfirmDialog/index.tsx
@@ -0,0 +1,47 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+
+interface DeleteConfirmDialogProps {
+  label: string;
+  deleteDialogOpen: boolean;
+  setDeleteDialogOpen: (open: boolean) => void;
+  handleDeleteConfirm: () => void;
+}
+
+export default function DeleteConfirmDialog({
+  label,
+  deleteDialogOpen,
+  setDeleteDialogOpen,
+  handleDeleteConfirm,
+}: DeleteConfirmDialogProps) {
+  return (
+    <AlertDialog open={deleteDialogOpen} onOpenChange={setDeleteDialogOpen}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete the
+            {` ${label} `} and remove all associated data.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={handleDeleteConfirm}
+            className="bg-red-600 hover:bg-red-700"
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
