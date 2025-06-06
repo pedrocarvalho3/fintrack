826c9ac0b31a297b932148ba2db9feaec16927ec
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-06-06 12:16:21 -0300

    style: :lipstick: incrementing space vertical in my dashboard cards

--- client/src/app/dashboard/components/DashboardGrid.tsx
+++ client/src/app/dashboard/components/DashboardGrid.tsx
@@ -3,7 +3,7 @@ import IncomeCard from './IncomeCard';
 
 export default function DashboardGrid() {
   return (
-    <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
       <IncomeCard />
       <ExpenseCard />
     </div>
