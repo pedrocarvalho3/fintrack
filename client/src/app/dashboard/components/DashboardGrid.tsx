ff19a87220d095960ec897232922be0d718dbf57
pedrocarvalho3 <pdrpvp0310@gmail.com>
2025-06-06 12:13:55 -0300

    feat: :sparkles: finalizing static expense card

--- client/src/app/dashboard/components/DashboardGrid.tsx
+++ client/src/app/dashboard/components/DashboardGrid.tsx
@@ -1,9 +1,11 @@
+import ExpenseCard from './ExpenseCard';
 import IncomeCard from './IncomeCard';
 
 export default function DashboardGrid() {
   return (
     <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
       <IncomeCard />
+      <ExpenseCard />
     </div>
   );
 }
